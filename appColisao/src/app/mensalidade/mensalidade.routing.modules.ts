57ffd1b6998d77e89db24bc0db3970e705662482
Bruno pc CCSP <brunoccsp@gmail.com>
2018-03-27 17:12:05 -0300

    arrumando o git

--- appColisao/src/app/mensalidade/mensalidade.routing.modules.ts
+++ appColisao/src/app/mensalidade/mensalidade.routing.modules.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { CanDeactivate } from '@angular/router/src/interfaces';
 import { Resolve } from '@angular/router/src/interfaces';
 import { Component, NgModule } from '@angular/core';
@@ -24,4 +25,32 @@ const mensalidadeRounting:Routes=[
     imports:[RouterModule.forChild(mensalidadeRounting)],
     exports:[RouterModule]
 })
+=======
+import { CanDeactivate } from '@angular/router/src/interfaces';
+import { Resolve } from '@angular/router/src/interfaces';
+import { Component, NgModule } from '@angular/core';
+import { MensalidadeComponent } from './mensalidade.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { MesesComponent } from './meses/meses.component';
+import { MensalidadeResolver } from './guard/mensalidade.resolve';
+import { MensalidadeFormComponent } from './mensalidade-form/mensalidade-form.component';
+import { MensalidadeDiactivatedGuard } from './mensalidade.diactivated.guard';
+
+const mensalidadeRounting:Routes=[
+    //adicionando rotas filhas
+    {path:'',component:MensalidadeComponent,
+        children:[
+            {path:'edit', component:MensalidadeFormComponent},
+            {path:':id',component:MesesComponent,
+                resolve:{mensalidade:MensalidadeResolver}
+            }           
+        ]
+    },
+];
+
+@NgModule({
+    imports:[RouterModule.forChild(mensalidadeRounting)],
+    exports:[RouterModule]
+})
+>>>>>>> f07fd22f033f9ee39d7eac9cef00ea92a5f5ff68
 export class MensalidadeRoutingModules{}
--- appColisao/src/app/mensalidade/meses/meses.component.ts
+++ appColisao/src/app/mensalidade/meses/meses.component.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Http, RequestOptions } from '@angular/http';
 import { Component, OnInit, ViewChild, ElementRef, Input, EventEmitter, Output } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
@@ -83,3 +84,90 @@ export class MesesComponent implements OnInit {
   
   
 }
+=======
+import { Http, RequestOptions } from '@angular/http';
+import { Component, OnInit, ViewChild, ElementRef, Input, EventEmitter, Output } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Meses } from '../meses';
+import { ActivatedRoute, Router } from '@angular/router';
+import { log } from 'util';
+import { MesesServices } from './../services/meses.services';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Observable } from 'rxjs/Observable';
+import { Jogadores } from '../jogadores';
+
+@Component({
+  selector: 'app-meses',
+  templateUrl: './meses.component.html',
+  styleUrls: ['./meses.component.css']
+})
+export class MesesComponent implements OnInit {
+  
+  
+  public inscricao:Subscription;
+  mes:Meses;
+  id_mes:number;
+  jogadores:any;
+  jog:Jogadores;
+  msg:boolean = false;
+  resultado:number = 0 ;
+  
+  constructor(private route:ActivatedRoute,
+              private mesesServices:MesesServices,
+              private router:Router,
+              private http:Http ) {}
+               
+  ngOnInit() {    
+   
+    this.inscricao = this.route.params.subscribe(
+      (params:any)=>{
+        let id = params['id'];
+        this.mes = this.mesesServices.getMes(id);
+        this.mesesServices.getMensalidade(id).subscribe(data => {
+          this.jogadores = data;
+          console.log(this.jogadores);
+          this.somaMensalidade();
+        });        
+      }     
+    );
+    //this.inscricao = this.route.data.subscribe((info)=>{this.mes = info.mes},(jog)=>{this.jogadores = jog.data});
+
+  }
+  /*updateMensalidade(idMes:number,valor:number,idMensalidade:number)
+  {
+    let teste =  this.mesesServices.updateMensalidade(idMes,valor, idMensalidade).subscribe();
+    return teste;
+  }*/
+
+  somaMensalidade()
+  {
+    let somaValor;
+    let soma:number= 0;
+    this.resultado = 0;
+    for(somaValor in this.jogadores) 
+    {              
+      soma = Number(this.jogadores[somaValor].Valor);
+      this.resultado += soma;
+    }
+    
+  }
+  updateMensalidade(jogador:Jogadores)
+  {     
+    this.mesesServices.updateMensalidade(jogador).subscribe((data)=>{  
+      this.jog = data;
+      console.log(this.jog);
+      //let intervalo = setTimeout(this.teste,3000);
+      this.msg = true;
+      setTimeout(()=>{
+        this.msg = false;
+      },2000);
+      //this.msg  = true;
+    },
+    (error)=>{console.log('Error'+error)});
+    //this.http.post('http://192.168.137.1/portifoliogithub/registro/app/php/mensalidade.php',jogador).subscribe((data)=>{console.log(jogador)},(error)=>{console.log('Error'+error)});
+    this.somaMensalidade(); 
+  }
+  
+  
+}
+>>>>>>> f07fd22f033f9ee39d7eac9cef00ea92a5f5ff68
