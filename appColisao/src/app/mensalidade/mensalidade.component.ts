0c087b05cd9c7ce790b4f76c14c72faa20d82b92
Bruno pc CCSP <brunoccsp@gmail.com>
2018-03-27 17:38:40 -0300

    arrumado o merge

--- appColisao/src/app/mensalidade/mensalidade.component.ts
+++ appColisao/src/app/mensalidade/mensalidade.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { MesesServices } from './services/meses.services';
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -47,53 +46,3 @@ export class MensalidadeComponent implements OnInit {
     this.router.navigate(['/mensalidade/edit']);
   }
 }
-=======
-import { MesesServices } from './services/meses.services';
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-
-@Component({
-  selector: 'app-mensalidade',
-  templateUrl: './mensalidade.component.html',
-  styleUrls: ['./mensalidade.component.css']
-})
-export class MensalidadeComponent implements OnInit {
-  @ViewChild('mySidebar') myDiv: ElementRef;
-  flag:boolean = false;
-  mensalidade:string = "Bem Vindo ao Controle de Mensalidade";
-  private meses:any[];
-  constructor(private mesesServices:MesesServices,
-              private route:ActivatedRoute,
-              private router:Router) { }
-
-  ngOnInit() 
-  {
-    this.menuAutomatico();
-    this.meses = this.mesesServices.getMeses();
-  }
-  closeMenu()
-  {
-    this.myDiv.nativeElement.style.display = "none";
-  }
-  openMenu()
-  { 
-    this.myDiv.nativeElement.style.display = "block";
-  }
-  menuAutomatico()
-  {
-    if(!this.flag)
-    {
-      this.closeMenu();
-      this.flag = true;
-    }else{
-      this.openMenu();
-      this.flag = false;
-    }
-  }
-  editarJogador()
-  {
-    //console.log("pooora");
-    this.router.navigate(['/mensalidade/edit']);
-  }
-}
->>>>>>> f07fd22f033f9ee39d7eac9cef00ea92a5f5ff68
